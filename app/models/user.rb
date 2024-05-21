3ede164734dfbfea0af31fc80905dde3d5ec11ca
saiko233 <luffy123824778@gmail.com>
2024-05-21 15:54:45 +0900

    user管理機能およびテスト実装

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # 新規登録/ユーザー情報
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
+
+  # 新規登録/本人情報確認
+  # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  # お名前カナ(全角)は、名字と名前がそれぞれ必須
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  # 生年月日必須
+  validates :birth_date, presence: true
+
+  # Association
+  has_many :items
+  has_many :purchases
+
+end
