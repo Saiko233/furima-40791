5687b37e7436cabadd51e159db02d5f3a4069b25
saiko233 <luffy123824778@gmail.com>
2024-05-23 10:15:22 +0900

    validationFixing

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, presence: true
-  validates :category_id, numericality: { other_than: 1}, presence: true
-  validates :condition_id, numericality: { other_than: 1}, presence: true
-  validates :shipping_fee_id, numericality: { other_than: 1}, presence: true
-  validates :prefecture_id, numericality: { other_than: 1}, presence: true
-  validates :shipping_day_id, numericality: { other_than: 1}, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
 
   def was_attached?
     self.image.attached?
