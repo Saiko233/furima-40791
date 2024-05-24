f701fa90a5287cd852b8ad90dd9edc1a53ccc47d
saiko233 <luffy123824778@gmail.com>
2024-05-24 10:33:28 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
 
   def index
     @items = Item.order(created_at: :desc)
   end
 
   def show
-
   end
 
   def new
@@ -21,7 +20,20 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
+  def edit
+    unless @item.user_id == current_user.id
+        redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
